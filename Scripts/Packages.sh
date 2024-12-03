43676e3cf05a9a5d97278eb278f338dd00031a18
Kylin <37763338+kylin-ink@users.noreply.github.com>
2024-12-03 19:32:49 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -41,9 +41,20 @@ UPDATE_PACKAGE "luci-app-gecoosac" "lwb1978/openwrt-gecoosac" "main"
 UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
 UPDATE_PACKAGE "luci-app-wolplus" "VIKINGYFY/packages" "main" "pkg"
 
-UPDATE_PACKAGE "lucky" "gdy666/luci-app-lucky" "main" "pkg"
-UPDATE_PACKAGE "luci-app-lucky" "gdy666/luci-app-lucky" "main" "pkg"
+#UPDATE_PACKAGE "lucky" "gdy666/luci-app-lucky" "main" "pkg"
+#UPDATE_PACKAGE "luci-app-lucky" "gdy666/luci-app-lucky" "main" "pkg"
 UPDATE_PACKAGE "luci-app-turboacc" "chenmozhijin/turboacc" "main" "pkg"
+UPDATE_PACKAGE "lucky" "kenzok8/small-package" "main" "pkg"
+UPDATE_PACKAGE "luci-app-lucky" "kenzok8/small-package" "main" "pkg"
+UPDATE_PACKAGE "luci-app-quickstart" "kenzok8/small-package" "main" "pkg"
+UPDATE_PACKAGE "luci-app-easymesh" "kenzok8/small-package" "main" "pkg"
+UPDATE_PACKAGE "luci-app-adguardhome" "kenzok8/small-package" "main" "pkg"
+UPDATE_PACKAGE "luci-app-socat" "kenzok8/small-package" "main" "pkg"
+UPDATE_PACKAGE "luci-app-store" "kenzok8/small-package" "main" "pkg"
+UPDATE_PACKAGE "lluci-lib-taskd" "kenzok8/small-package" "main" "pkg"
+UPDATE_PACKAGE "luci-lib-xterm" "kenzok8/small-package" "main" "pkg"
+UPDATE_PACKAGE "taskd" "kenzok8/small-package" "main" "pkg"
+UPDATE_PACKAGE "quickstart" "kenzok8/small-package" "main" "pkg"
 
 
 if [[ $WRT_REPO != *"immortalwrt"* ]]; then
@@ -94,8 +105,8 @@ UPDATE_VERSION "sing-box"
 UPDATE_VERSION "tailscale"
 
 #删除官方的默认插件
-#rm -rf feeds/luci/applications/#luci-app-{passwall,mosdns,dockerman,dae*,bypass*}
-#rm -rf feeds/packages/net/{shadowsocks-rust,shadowsocksr-libev,xray*,v2ray*,dae*,sing-box}
+rm -rf feeds/luci/applications/#luci-app-{passwall,mosdns,dockerman,dae*,bypass*}
+rm -rf feeds/packages/net/{shadowsocks-rust,shadowsocksr-libev,xray*,v2ray*,dae*,sing-box}
 #git clone https://github.com/davidtall/small small
 
 #git clone https://github.com/zzsj0928/luci-app-pushbot luci-app-pushbot
