295a75caf9c5f671fd9fa33ec030e12a5fbbcdf0
Kylin <37763338+kylin-ink@users.noreply.github.com>
2024-12-04 15:23:38 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -107,6 +107,6 @@ UPDATE_VERSION "tailscale"
 #删除官方的默认插件
 rm -rf feeds/luci/applications/luci-app-{passwall,mosdns,dockerman,dae*,bypass*}
 rm -rf feeds/packages/net/{shadowsocks-rust,shadowsocksr-libev,xray*,v2ray*,dae*,sing-box}
-git clone https://github.com/davidtall/small small
+#git clone https://github.com/davidtall/small small
 
-git clone https://github.com/zzsj0928/luci-app-pushbot luci-app-pushbot
+#git clone https://github.com/zzsj0928/luci-app-pushbot luci-app-pushbot
