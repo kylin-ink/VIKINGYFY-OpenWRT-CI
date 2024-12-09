7db862cbe4da4a4ac7737f7987089ef2ab37423f
Kylin <37763338+kylin-ink@users.noreply.github.com>
2024-12-09 22:05:16 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -40,9 +40,29 @@ UPDATE_PACKAGE "luci-app-gecoosac" "lwb1978/openwrt-gecoosac" "main"
 UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
 UPDATE_PACKAGE "luci-app-wolplus" "VIKINGYFY/packages" "main" "pkg"
 
+#UPDATE_PACKAGE "lucky" "gdy666/luci-app-lucky" "main" "pkg"
+#UPDATE_PACKAGE "luci-app-lucky" "gdy666/luci-app-lucky" "main" "pkg"
+#UPDATE_PACKAGE "luci-app-turboacc" "chenmozhijin/turboacc" "main" "pkg"
+UPDATE_PACKAGE "lucky" "kenzok8/small-package" "main" "pkg"
+UPDATE_PACKAGE "luci-app-lucky" "kenzok8/small-package" "main" "pkg"
+UPDATE_PACKAGE "luci-app-nekobox" "kenzok8/small-package" "main" "pkg"
+#UPDATE_PACKAGE "luci-app-quickstart" "kenzok8/small-package" "main" "pkg"
+#UPDATE_PACKAGE "luci-app-easymesh" "kenzok8/small-package" "main" "pkg"
+#UPDATE_PACKAGE "luci-app-adguardhome" "kenzok8/small-package" "main" "pkg"
+UPDATE_PACKAGE "luci-app-socat" "kenzok8/small-package" "main" "pkg"
+#UPDATE_PACKAGE "luci-lib-xterm" "kenzok8/small-package" "main" "pkg"
+#UPDATE_PACKAGE "taskd" "kenzok8/small-package" "main" "pkg"
+#UPDATE_PACKAGE "luci-lib-taskd" "kenzok8/small-package" "main" "pkg"
+#UPDATE_PACKAGE "luci-app-store" "kenzok8/small-package" "main" "pkg"
+#UPDATE_PACKAGE "quickstart" "kenzok8/small-package" "main" "pkg"
+
 if [[ $WRT_REPO != *"immortalwrt"* ]]; then
 	UPDATE_PACKAGE "qmi-wwan" "immortalwrt/wwan-packages" "master" "pkg"
 fi
+
+# 替换成最新版的golang
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
 
 #更新软件包版本
 UPDATE_VERSION() {
@@ -81,3 +101,10 @@ UPDATE_VERSION() {
 #UPDATE_VERSION "软件包名" "测试版，true，可选，默认为否"
 UPDATE_VERSION "sing-box"
 UPDATE_VERSION "tailscale"
+#删除官方的默认插件
+rm -rf feeds/luci/applications/luci-app-{passwall,mosdns,dockerman,dae*,bypass*}
+rm -rf feeds/packages/net/{shadowsocks-rust,shadowsocksr-libev,xray*,v2ray*,dae*,sing-box}
+
+git clone https://github.com/davidtall/small small
+
+git clone https://github.com/zzsj0928/luci-app-pushbot luci-app-pushbot
